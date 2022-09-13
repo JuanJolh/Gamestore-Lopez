746709fb94c63805f32a1c14396d0e8ac10c4605
Juan Lopez <jjlop312@gmail.com>
2022-09-13 17:46:41 -0500

    ItemDetail v2

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import ItemDetail from "./ItemDetail";
+import { productos } from "./productos";
 
 const ItemDetailContainer = () => {
     const [item, setItem] = useState({});
 
     useEffect(() => {
-        const productos = [
-            {"id":1, "nombre":"Xbox Series X", "marca":"Microsoft", "imagen":"https://compass-ssl.xbox.com/assets/b9/0a/b90ad58f-9950-44a7-87fa-1ee8f0b6a90e.jpg?n=XSX_Page-Hero-0_768x792.jpg", "precio":"499", "descripcion":"Presentamos Xbox Series X, la consola Xbox más rápida y potente de la historia. Juega miles de títulos de cuatro generaciones de consolas"},
-        ];
         const getProduct = () =>
             new Promise((resolve, reject) => {
                 const product = productos.find((prod) => prod.id === 1)
